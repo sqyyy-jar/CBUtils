a12b18c3083c58fe48c892f1dc5c8f60de799075
Ben Siebert <dev@mctzock.de>
2021-02-28 17:24:16 +0100

    Created Sign Command

--- src/main/java/net/craftions/cbutils/command/CommandHead.java
+++ src/main/java/net/craftions/cbutils/command/CommandHead.java
@@ -1,14 +1,26 @@
 package net.craftions.cbutils.command;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommandHead implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if(sender instanceof Player){
+
+        }else {
+            sender.sendMessage("You need to be a player.");
+        }
         return true;
     }
 }
--- src/main/java/net/craftions/cbutils/command/CommandSign.java
+++ src/main/java/net/craftions/cbutils/command/CommandSign.java
@@ -1,13 +1,42 @@
 package net.craftions.cbutils.command;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommandSign implements CommandExecutor {
 
     @Override
     public boolean onCommand(@org.jetbrains.annotations.NotNull CommandSender sender, @org.jetbrains.annotations.NotNull Command command, @org.jetbrains.annotations.NotNull String label, @org.jetbrains.annotations.NotNull String[] args) {
+        if(sender instanceof Player){
+            Player p = (Player) sender;
+            if(p.getItemOnCursor().getType() != Material.AIR){
+                String newLore = "";
+                for(int i = 0; i < args.length; i++){
+                    newLore += args[i] + "";
+                }
+                ItemStack i = p.getItemOnCursor();
+                ItemMeta meta = i.getItemMeta();
+                List<String> lore = new ArrayList<>();
+                lore.add("");
+                lore.add("§cSigniert von §e" + p.getName());
+                lore.add("§f" + newLore);
+                i.setItemMeta(meta);
+                p.setItemOnCursor(i);
+                p.sendMessage("§2Das Item wurde erfolgreich signiert!");
+            }else {
+                p.sendMessage("§cBitte halte das Item, dass du signieren willst in der Haupt-Hand.");
+            }
+        }else {
+            sender.sendMessage("You need to be a player.");
+        }
         return true;
     }
 }
