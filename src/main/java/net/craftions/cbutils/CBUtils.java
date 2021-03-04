fefda256d30e3446bac19fb894867fc4d2b5dfc2
MCTzOCK <dev@mctzock.de>
2021-03-04 08:27:21 +0100

    Added some cool stuff!

--- src/main/java/net/craftions/cbutils/CBUtils.java
+++ src/main/java/net/craftions/cbutils/CBUtils.java
@@ -2,12 +2,17 @@ package net.craftions.cbutils;
 
 import net.craftions.cbutils.command.CommandHead;
 import net.craftions.cbutils.command.CommandSign;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CBUtils extends JavaPlugin {
 
+    public static Economy econ = null;
+
     @Override
     public void onEnable() {
+        setupEconomy();
         getCommand("signieren").setExecutor(new CommandSign());
         getCommand("kopf").setExecutor(new CommandHead());
 
@@ -16,5 +21,17 @@ public final class CBUtils extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
     }
 }
--- src/main/java/net/craftions/cbutils/command/CommandHead.java
+++ src/main/java/net/craftions/cbutils/command/CommandHead.java
@@ -1,28 +1,28 @@
 package net.craftions.cbutils.command;
 
+import net.craftions.cbutils.CBUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class CommandHead implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if(sender instanceof Player){
-            if(args.length == 1){
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "minecraft:give " + sender.getName() + " minecraft:player_head{SkullOwner:" + args[0] + "}");
-                sender.sendMessage("§2Du hast den Kopf von §2" + args[0] + "§2 erhalten!");
-            }else{
-                sender.sendMessage("§cBitte nutze /kopf <spielername>");
+            if(CBUtils.econ.getBalance((Player) sender) >= 100){
+                if(args.length == 1){
+                    CBUtils.econ.withdrawPlayer((Player) sender, -100);
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "minecraft:give " + sender.getName() + " minecraft:player_head{SkullOwner:" + args[0] + "}");
+                    sender.sendMessage("§2Du hast den Kopf von §2" + args[0] + "§2 erhalten!");
+                }else{
+                    sender.sendMessage("§cBitte nutze /kopf <spielername>");
+                }
+            }else {
+                sender.sendMessage("§cDu brauchst mindestens $ 100");
             }
         }else {
             sender.sendMessage("You need to be a player.");
